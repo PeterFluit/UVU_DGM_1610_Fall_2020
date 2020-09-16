f15428286ee08f7b4db1be154d7e347448081aef
Peter Fluit <fluitpd@gmail.com>
2020-09-16 14:59:05 -0600

    Worked more on PlayerController to get it to turn

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 20;
+    public int speed = 2;
+    public float turnSpeed;
+
+    public float horizontalInput;
+    public float vInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      horizontalInput = Input.GetAxis("Horizontal");
+      vInput = Input.GetAxis("Vertical");
+
       //Move the vehicle forward
-      transform. Translate(Vector3.forward * Time.deltaTime * speed);  
+      transform.Translate(Vector3.forward * Time.deltaTime * vInput * speed);
+      transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnSpeed);  
     }
 }
