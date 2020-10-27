add42eae80334d2353b715e0edce49ec2410e919
Peter Fluit <fluitpd@gmail.com>
2020-10-27 12:45:38 -0600

    Challenge 2
    Along with changes to Challenge 2

--- Feed The Sheep/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Feed The Sheep/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
-
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
@@ -25,8 +24,11 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        int ballIndex = Random.Range(0,3);
+        
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
     }
 
 }
