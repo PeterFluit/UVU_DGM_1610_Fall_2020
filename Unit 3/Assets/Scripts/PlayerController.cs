30894b53891bd6bee5d5f79ea0f9502471585632
Peter Fluit <fluitpd@gmail.com>
2020-11-02 19:51:19 -0700

    Adjusted the jump force and gravity

--- Unit 3/Assets/Scripts/PlayerController.cs
+++ Unit 3/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 100);
+        Physics.gravity *= gravityModifier;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
